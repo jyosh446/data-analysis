c125992db16dbc9a2e623c42ea7302ee6e630db9
Jyoshna Vallabhaneni <99242717+jyosh446@users.noreply.github.com>
2022-05-29 16:23:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from PIL import Image
 from multiapp import MultiApp
-from apps import eda1, eda2, model, home
+from apps import da1, da2, model, home
 
-image1 = Image.open("car_eda3.jpg")
+image1 = Image.open("car.jpg")
 st.set_page_config(
     page_title="Data Analysis",
     page_icon=image1,
@@ -14,9 +14,9 @@ st.set_page_config(
 apps = MultiApp()
 
 apps.add_app("Home", home.app)
-apps.add_app("Exploratory Data Analysis on car data by CarDekho", eda1.app)
-apps.add_app("Exploratory Data Analysis(EDA)", eda2.app)
-apps.add_app("Car Selling Price Prediction", model.app)
+apps.add_app(" Data Analysis on car dataset", da1.app)
+apps.add_app("Data Analysis", da2.app)
+apps.add_app("Car Predictions", model.app)
 
 # The main app
 apps.run()
