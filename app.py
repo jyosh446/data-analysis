477a9306a5a2dab97ccecdba60680782489f17d8
Jyoshna Vallabhaneni <99242717+jyosh446@users.noreply.github.com>
2022-05-29 16:25:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from PIL import Image
 from multiapp import MultiApp
 from apps import da1, da2, model, home
 
-image1 = Image.open("car.jpg")
+image1 = Image.open("images.jpg")
 st.set_page_config(
     page_title="Data Analysis",
     page_icon=image1,
